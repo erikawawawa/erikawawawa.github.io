57b19e897117e85bd86a6e8d25c87ad357b6dae4
erika <e.wang1502@gmail.com>
2023-09-01 15:10:57 -0700

    Added case studies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import { register } from "swiper/element/bundle";
+
+register();
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+  ]
+});
